751860c5c23a0f262ec4c482a7ea9951ad6f568e
WeitingChen <chenw@usi.ch>
2024-03-20 13:34:55 +0100

    fix windows setup bugs

--- .config/install.ps1
+++ .config/install.ps1
@@ -1,11 +1,10 @@
 # Optional: Remove .git folder inside nvim folder
 
-Remove-Item $env:LOCALAPPDATA\nvim -Recurse -Force
-Remove-Item $env:LOCALAPPDATA\nvim-data -Recurse -Force
+Remove-Item $env:USERPROFILE\AppData\Local\nvim -Recurse -Force
+Remove-Item $env:USERPROFILE\AppData\Local\nvim-data -Recurse -Force
 
 # link config file to user dir
 New-Item -Path $env:USERPROFILE\AppData\Local\nvim\ -ItemType SymbolicLink -Value $env:USERPROFILE\myDotfiles\.config\nvim\ -Force
 New-Item -Path "C:\Program Files\WezTerm\wezterm.lua" -ItemType SymbolicLink -Value $env:USERPROFILE\myDotfiles\wezterm_win\wezterm.lua -Force
 
-New-Item -Path $env:USERPROFILE\AppData\Local\.vim\.vimrc -ItemType SymbolicLink -Value 
-$env:USERPROFILE\myDotfiles\.vimrc -Force
+New-Item -Path $env:USERPROFILE\.vim\.vimrc -ItemType SymbolicLink -Value $env:USERPROFILE\myDotfiles\.vimrc -Force
