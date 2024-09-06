41ee52583e12c2855c745356fb9fc111ac184e73
WeiTing Chen <>
2024-09-06 11:56:08 +0200

    new version update

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,18 +1,35 @@
 local builtin = require "telescope.builtin"
+local utils = require "telescope.utils"
 
 -- setup
 require("telescope").setup {
-  layout_config = {
-    horizontal = {
-      prompt_position = "bottom",
-      preview_width = 0.65,
+  defaults = {
+    vimgrep_arguments = {},
+    layout_config = {
+      horizontal = {
+        prompt_position = "bottom",
+        preview_width = 0.65,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.85,
+      height = 0.90,
+      preview_cutoff = 80,
     },
-    vertical = {
-      mirror = false,
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+      find_command = {
+        "rg",
+        "-L",
+        "--files",
+        "--hidden",
+        "--glob",
+        "!**/.git/*",
+      },
     },
-    width = 0.85,
-    height = 0.90,
-    preview_cutoff = 80,
   },
 }
 
@@ -22,3 +39,6 @@ pcall(require("telescope").load_extension, "fzf")
 vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
 vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find file in buffer" })
+vim.keymap.set("n", "<space>ff", function()
+  builtin.find_files { cwd = utils.buffer_dir() }
+end, { desc = "file browser in buffer" })
