59b1358c1290313bafb782b0e78b388efb971ed0
WeiTing Chen <>
2024-10-17 18:31:19 +0200

    clearup windows setting:

--- .config/install.ps1
+++ .config/install.ps1
@@ -13,8 +13,10 @@ Remove-Item $env:USERPROFILE\AppData\Local\nvim -Recurse -Force
 Remove-Item $env:USERPROFILE\AppData\Local\nvim-data -Recurse -Force
 
 # link config file to user dir
-New-Item -Path $env:USERPROFILE\AppData\Local\nvim\ -ItemType SymbolicLink -Value $env:USERPROFILE\.mydotfiles\.config\nvim\ -Force
+New-Item -Path $env:USERPROFILE\AppData\Local\nvim\ -ItemType SymbolicLink -Value $env:USERPROFILE\.dotfiles\.config\nvim\ -Force
+New-Item -Path $env:USERPROFILE\AppData\Roaming\Zed\ -ItemType SymbolicLink -Value $env:USERPROFILE\.dotfiles\.config\zed\ -Force
 New-Item -Path "C:\Program Files\WezTerm\wezterm.lua" -ItemType SymbolicLink -Value $env:USERPROFILE\.mydotfiles\.config\wezterm_win\wezterm.lua -Force
-New-Item -Path $env:USERPROFILE\.emacs.d\ -ItemType SymbolicLink -Value $env:USERPROFILE\.dotfiles\.emacs.d\ -Force
+#New-Item -Path $env:USERPROFILE\.emacs.d\ -ItemType SymbolicLink -Value $env:USERPROFILE\.dotfiles\.emacs.d\ -Force
+#New-Item -Path $env:USERPROFILE\.doom.d\ -ItemType SymbolicLink -Value $env:USERPROFILE\.dotfiles\.doom.d\ -Force
 
 # New-Item -Path $env:USERPROFILE\.vim\.vimrc -ItemType SymbolicLink -Value $env:USERPROFILE\myDotfiles\.vimrc -Force
