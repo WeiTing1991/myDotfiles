4d4db333072b7ff594bc33d6311925bb78edd4db
WeiTing Chen <>
2024-10-22 21:31:09 +0200

    emacs setup clean up”

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -128,13 +128,13 @@
 
 (require 'config)
 
-(load-file (expand-file-name "./config/core.el" user-emacs-directory))
-(load-file (expand-file-name "./config/ui.el" user-emacs-directory))
-(load-file (expand-file-name "./config/cmd-system.el" user-emacs-directory))
-(load-file (expand-file-name "./config/file-system.el" user-emacs-directory))
-(load-file (expand-file-name "./config/lsp.el" user-emacs-directory))
-(load-file (expand-file-name "./config/app.el" user-emacs-directory))
-(load-file (expand-file-name "./config/terminals.el" user-emacs-directory))
+;;(load-file (expand-file-name "./config/core.el" user-emacs-directory))
+;;(load-file (expand-file-name "./config/ui.el" user-emacs-directory))
+;;(load-file (expand-file-name "./config/cmd-system.el" user-emacs-directory))
+;;(load-file (expand-file-name "./config/file-system.el" user-emacs-directory))
+;;(load-file (expand-file-name "./config/lsp.el" user-emacs-directory))
+;;(load-file (expand-file-name "./config/app.el" user-emacs-directory))
+;;(load-file (expand-file-name "./config/terminals.el" user-emacs-directory))
 
 ;; (defun my-persp-format ()
 ;;   "Return the name of the current perspective."
