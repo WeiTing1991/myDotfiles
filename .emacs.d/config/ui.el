f9ca8d50325038be8c78f2c332211482f440c7a7
WeiTing Chen <>
2024-10-24 04:55:43 +0200

    emacs setup clean up”

--- .emacs.d/config/ui.el
+++ .emacs.d/config/ui.el
@@ -1,10 +1,6 @@
 ;;; ui.el
 
 ;;; code:
-(use-package all-the-icons
-  :straight (all-the-icons :type git :host github :repo "domtronn/all-the-icons.el" :branch "svg" :files (:defaults "svg"))
-  :if (display-graphic-p)
-  )
 
 ;; indent mode and highlight
 ;; check https://github.com/DarthFennec/highlight-indent-guides
