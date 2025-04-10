649bc04299b4fa5c58f06de7969d51672ad30956
WeitingChen <weitingchen0502@gmail.com>
2025-04-10 10:37:08 +0200

    oh my posh

--- .config/windows/weitingchen.ps1
+++ .config/windows/weitingchen.ps1
@@ -1,4 +1,28 @@
+# Import the PSReadLine module
 Import-Module PSReadLine
+
+# Set predictive IntelliSense (PowerShell 7.2+ feature)
+Set-PSReadLineOption -PredictionSource History
+
+# Ctrl+Space for IntelliSense suggestions
+Set-PSReadLineKeyHandler -Key Ctrl+tab -Function Complete
+
+# Use AcceptSuggestion instead of Complete
+Set-PSReadLineKeyHandler -Key Ctrl+f -Function AcceptSuggestion
+
+# Tab completion
+#Set-PSReadLineKeyHandler -Key tab -Function MenuComplete
+
+# Searching history with up/down arrows
+Set-PSReadLineKeyHandler -Key Ctrl+p -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key Ctrl+n -Function HistorySearchForward
+
+# Ctrl+d to exit, like in bash
+Set-PSReadLineKeyHandler -Key Ctrl+w -Function DeleteCharOrExit
+
+# Initialize Oh-My-Posh with a theme
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
+
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 Set-Location $HOME
 
