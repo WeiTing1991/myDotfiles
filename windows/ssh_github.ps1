b4e5c9cc1642a5a7076efdec9de9828dcb1901c0
WeiTing1991 <72130405+WeiTing1991@users.noreply.github.com>
2026-01-08 00:32:07 +0100

    fix: gitconfig with email

--- windows/ssh_github.ps1
+++ windows/ssh_github.ps1
@@ -1,4 +1,4 @@
-# ssh-keygen -t ed25519 -C "weitingchen0502@gmail.com"
+#ssh-keygen -t ed25519 -C "weitingchen0502@gmail.com"
 ssh-keygen -t ed25519 -C "weiting@manukai.ch"
 cat ~/.ssh/id_ed25519.pub
 
