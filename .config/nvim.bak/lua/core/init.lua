571ca9ac22f2ccb32bdc74ec948f7e2ee4b31ca3
WeitingChen <72130405+WeiTing1991@users.noreply.github.com>
2026-08-16 19:58:33 +0200

    chore: remove nvim.bak

---
+++ .config/nvim.bak/lua/core/init.lua
@@ -0,0 +1,75 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+require("core.options")
+require("core.keymaps")
+require("core.autocmds")
+
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    { import = "plugins.default" },
+    { import = "plugins.ui" },
+    { import = "plugins.editor" },
+    { import = "plugins.snack" },
+    { import = "plugins.lsp" },
+    { import = "plugins.lsp_enhance" },
+    { import = "plugins.git" },
+    { import = "plugins.ai" },
+    { import = "plugins.tools" },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  ui = {
+    border = "rounded",
+    size = { width = 0.8, height = 0.8 },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+dofile(vim.g.base46_cache .. "syntax")
+dofile(vim.g.base46_cache .. "treesitter")
+
+require("user_keymaps")
---
+++ .config/nvim.bak/lua/lsp/formater_linter.lua
@@ -0,0 +1,26 @@
+-- formate and linter
+local M = {}
+
+M = {
+  -- formatter
+  lua = { "stylua" },
+  python = { "ruff" },
+
+  json = { "prettier" },
+  -- { "prettierd" },
+  github = { "actionlint" },
+  sh = { "shfmt" },
+  c = { "clang-format" },
+  cpp = { "clang-format" },
+
+  cSharp = { "csharpier" },
+  -- typescript = { "biome" },
+
+  -- linter
+  -- javascript = { "eslint_d" },
+  -- typescript = { "eslint_d" },
+  -- javascriptreact = { "eslint_d" },
+  -- typescriptreact = { "eslint_d" },
+}
+
+return M
